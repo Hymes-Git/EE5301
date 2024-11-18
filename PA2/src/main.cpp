bed66f1978ae517f4a72526d6cca76b7678d59fc
Lily <gahymes@gmail.com>
2024-11-18 14:25:58 -0600

    started pa2

---
+++ PA2/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+bool debug = false;
+
+int main(int argc, char* argv[]) {
+
+    string fileName;
+
+    if (argc < 2) {
+        cout << "Error: Fewer than 2 arguments" << endl;
+        return -1;
+    } else {
+        fileName = argv[1];
+    }
+
+    if (debug)
+        cout << "Running with files: "
+
+}
