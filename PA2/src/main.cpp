a28cfc6411899b035a8ec115e5cc7032171c008a
Lily <gahymes@gmail.com>
2024-11-20 11:11:35 -0600

    swap mains

--- PA2/src/main.cpp
+++ PA2/src/main.cpp
@@ -1,25 +1,55 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+// written by Suraj Dalvi, ECE Dept., Univ. of Minnesota
+// Heavily modified by Kia Bazargan (renaming variables, adding
+// comments, creating a clean version of the hyperedge data structure)
+
+
+#include<iostream>
+#include<stdio.h>
+#include<map>
+#include<vector>
+#include <string.h>
+#include "suraj_parser.h"
 
 using namespace std;
 
-bool debug = false;
+	
+int main(int argv, char *argc[])
+{
+	char inareFileName[100];
+	char innetFileName[100];
+	char inPadLocationFileName[100];
 
-int main(int argc, char* argv[]) {
+	if (argv!=2) {
+		cout << "Please provide a circuit file name with no extension." << endl;
+		return 1;
+	}
+        	     
+    cout << "Reading circuit file " << argc[1] << endl;
 
-    string fileName;
+	strcpy (inareFileName, argc[1]);
+	strcat(inareFileName, ".are");
+	strcpy(innetFileName,argc[1]);
+	strcat(innetFileName,".net");
+	strcpy(inPadLocationFileName,argc[1]);
+	strcat(inPadLocationFileName,".kiaPad");
 
-    if (argc < 2) {
-        cout << "Error: Fewer than 2 arguments" << endl;
-        return -1;
-    } else {
-        fileName = argv[1];
-    }
+	int success = parseIbmFile(inareFileName, innetFileName, inPadLocationFileName);
+	if (success == -1) {
+		cout << "Error reading input file(s)" << endl;
+		return 0;
+	}
 
-    if (debug)
-        cout << "Running with files: " << fileName << endl;
+	printf("\nNumber of vertices,hyper = %d %d\n",numCellsAndPads,numhyper);
+	
 
-    return 0;
+	
+	// call function(s) dealing with creating the Q matrix, placement, etc.
 
+
+	
+	
+    free(pinLocations);
+	free(hEdge_idxToFirstEntryInPinArray);
+	free(cellPinArray);
+	free(hyperwts);
 }
